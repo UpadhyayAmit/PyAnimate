bce55acd74d74d64dc2b292400fb19f9df26edd0
Amit Upadhyay <connect@amitupadhyay.com>
2026-04-14 23:23:09 +0400

    fix: correct Twitter link and add personal site to social links

--- components/shared-footer.tsx
+++ components/shared-footer.tsx
@@ -32,11 +32,20 @@ const SOCIAL_LINKS = [
     ),
   },
   {
-    href: 'https://x.com/amitupadhyayai',
+    href: 'https://x.com/amitupadhyay_ai',
     label: 'X / Twitter',
     icon: (
       <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
         <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.747l7.73-8.835L1.254 2.25H8.08l4.253 5.622zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+      </svg>
+    ),
+  },
+  {
+    href: 'https://amitupadhyay.com',
+    label: 'Personal Site',
+    icon: (
+      <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z" />
       </svg>
     ),
   },
